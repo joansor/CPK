889051a07d40aa0afd520db899692b75a207e983
Simon <marquez-simon@hotmail.fr>
2021-02-08 12:00:52 +0100

    insertNavbar

---
+++ index - navbar.php
@@ -0,0 +1,57 @@
+<html>
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+<!-- MDB -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.1.0/mdb.min.css" rel="stylesheet"/>
+
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.1.0/mdb.min.js"></script>
+
+<!--CSS Perso-->
+<link href="style_perso.css" rel="stylesheet"/>
+
+<body>
+	
+	<nav class="navbar navbar-expand-md navbar-light bg-light">
+		<div class="container-fluid">
+			
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-mdb-toggle="collapse"
+				data-mdb-target="#Topmenu"
+				aria-controls="Topmenu"
+				aria-extended="false"
+				aria-label="Menu mobile"
+				title="Menu mobile"
+			>
+      	<i class="fas fa-bars"></i>
+    	</button>
+			
+			<span class="navbar-brand">CPK</span>
+			
+			<div class="collapse navbar-collapse justify-content-center" id="Topmenu">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Accueil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Infos Pratique</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Les Equipes</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Espace Membre</a>
+					</li>
+				</ul>
+			</div>
+			
+		</div>
+	</nav>
+	
+</body>
+
+</html>
