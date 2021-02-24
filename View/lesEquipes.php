39bfd6a2a08dedcf20d12ac348a9528ff343a2c4
SOR <sor.joan@gmail.com>
2021-02-24 23:12:04 +0100

    commencement de la page equipes

--- View/lesEquipes.php
+++ View/lesEquipes.php
@@ -1,1 +1,14 @@
-coucou je suis Les Equipes
+<div class="container col-lg-12 col-md-12 col-sd-12">
+    <h1>LES ÉQUIPES</h1>
+    <div class="cercle"></div>
+    <h2>Sénior</h2>
+    <div>
+
+
+    </div>
+
+
+
+
+
+</div>
