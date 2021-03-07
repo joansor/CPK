60194ac8c838f3514eb33a00e5d3a805be09f72d
SOR <sor.joan@gmail.com>
2021-03-07 01:29:38 +0100

    requete et affichage du membre qui appartient à son équipe et categorie

--- View/lesEquipes.php
+++ View/lesEquipes.php
@@ -1,15 +1,12 @@
-<?php// include("pdo.php") ?>
 <h1>LES ÉQUIPES</h1>
 <br>
 <br>
 <?php
-// $requete = "SELECT equipes.EquipeNom,equipes.categories, equipes.imageEquipe, membres.nomMembre,membres.prenomMembre,joue.isCapitaine FROM `equipes` INNER JOIN joue ON joue.Equipes_idEquipe = equipes.idEquipe INNER JOIN membres ON membres.idMembres = joue.Membres_idMembres";
-$requeteListEquipe = "SELECT `categories`,`EquipeNom`,`imageEquipe` FROM `equipes`";
 
-$requeteMembreEquipe = "SELECT joue.Membres_idMembres,membres.nomMembre,membres.prenomMembre,joue.isCapitaine,joue.Equipes_idEquipe FROM `joue` INNER JOIN equipes ON joue.Equipes_idEquipe = equipes.idEquipe INNER JOIN membres ON membres.idMembres = joue.Membres_idMembres";
+$requeteListEquipe = "SELECT * FROM `equipes`";
 
 $executeListEquipe = $dbh->query($requeteListEquipe);
-$executeMembreEquipe = $dbh->query($requeteMembreEquipe);
+
 while ($resultListEquipe = $executeListEquipe->fetch()) {
 ?>
 
@@ -26,12 +23,19 @@ while ($resultListEquipe = $executeListEquipe->fetch()) {
             <hr id="separation">
             <div class="col-md-5 col-lg-5">
                 <div><?php echo $resultListEquipe['EquipeNom']; ?></div>
+
                 <?php
-                while ($resultMembreEquipe = $executeMembreEquipe->fetch()) {
-                    if ($resultMembreEquipe['Equipes_idEquipe'] === $resultMembreEquipe['Membres_idMembres']) { ?>
-                        <div><?php echo $resultMembreEquipe['nomMembre'] ?> </div>
-                        <div><?php echo $resultMembreEquipe['prenomMembre']; ?></div>
-                <?php }
+                
+                $idEquipe = $resultListEquipe['idEquipe'];
+
+                $requeteMembreEquipe = "SELECT membres.nomMembre, membres.prenomMembre FROM `membres` INNER JOIN joue ON joue.Membres_idMembres = membres.idMembres INNER JOIN equipes ON equipes.idEquipe = joue.Equipes_idEquipe WHERE joue.Equipes_idEquipe =".$idEquipe;
+
+                $executeMembreEquipe = $dbh->query($requeteMembreEquipe);
+
+                while ($resultMembreEquipe = $executeMembreEquipe->fetch()) { ?>
+                        
+                        <div><?php echo $resultMembreEquipe['nomMembre']." ".$resultMembreEquipe['prenomMembre']; ?> </div>
+                <?php
                 } ?>
             </div>
         </div>
