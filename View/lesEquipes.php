163cccff939bf9a4e6de23a165acf98707e89718
SOR <sor.joan@gmail.com>
2021-03-07 02:55:56 +0100

    ajout condition pour le EstCapitaine et css/html

--- View/lesEquipes.php
+++ View/lesEquipes.php
@@ -2,14 +2,11 @@
 <br>
 <br>
 <?php
+$requeteListEquipe = "SELECT * FROM `equipes`"; // Récupere la liste des équipes
+$executeListEquipe = $dbh->query($requeteListEquipe); // Execute la requête
 
-$requeteListEquipe = "SELECT * FROM `equipes`";
-
-$executeListEquipe = $dbh->query($requeteListEquipe);
-
-while ($resultListEquipe = $executeListEquipe->fetch()) {
+while ($resultListEquipe = $executeListEquipe->fetch()) { // Parcours les résultats de la requête
 ?>
-
     <div class="sous-titre">
         <div class="cercle"></div>
         <h2><?php echo $resultListEquipe['categories'] ?></h2>
@@ -19,24 +16,32 @@ while ($resultListEquipe = $executeListEquipe->fetch()) {
         <div class="row">
             <div class="col-md-5 col-lg-5">
                 <img id="photoGroup" src="<?php echo $resultListEquipe['imageEquipe']; ?>" alt="photo Groupe">
+                <!--Récupère l'image de chaque équipe-->
             </div>
             <hr id="separation">
             <div class="col-md-5 col-lg-5">
-                <div><?php echo $resultListEquipe['EquipeNom']; ?></div>
-
+                <h3 class="mt-4"><?php echo $resultListEquipe['EquipeNom']; //Récupère le nom de chaque équipe 
+                        ?></h3>
                 <?php
-                
-                $idEquipe = $resultListEquipe['idEquipe'];
+                $idEquipe = $resultListEquipe['idEquipe']; // récupère l'id de l'equipe
 
-                $requeteMembreEquipe = "SELECT membres.nomMembre, membres.prenomMembre FROM `membres` INNER JOIN joue ON joue.Membres_idMembres = membres.idMembres INNER JOIN equipes ON equipes.idEquipe = joue.Equipes_idEquipe WHERE joue.Equipes_idEquipe =".$idEquipe;
+                // Requête pour la liste des membres de l'équipe par rapport à l'id de l'équipe
+                $requeteMembreEquipe = "SELECT membres.nomMembre, membres.prenomMembre,joue.isCapitaine FROM `membres` INNER JOIN joue ON joue.Membres_idMembres = membres.idMembres INNER JOIN equipes ON equipes.idEquipe = joue.Equipes_idEquipe WHERE joue.Equipes_idEquipe =" . $idEquipe;
 
                 $executeMembreEquipe = $dbh->query($requeteMembreEquipe);
 
                 while ($resultMembreEquipe = $executeMembreEquipe->fetch()) { ?>
-                        
-                        <div><?php echo $resultMembreEquipe['nomMembre']." ".$resultMembreEquipe['prenomMembre']; ?> </div>
-                <?php
-                } ?>
+
+                    
+                        <div class ="row mt-3">
+                            <div id="nomMembre" class="col-md-5"><?php
+                            echo $resultMembreEquipe['nomMembre'] . " " . $resultMembreEquipe['prenomMembre'];?> </div>
+                           <?php  if ($resultMembreEquipe['isCapitaine'] === "1") { ?>
+                                <div id="capt" class ="col-md-1">C</div>
+                            <?php } ?>
+                        </div>
+                   
+                <?php } ?>
             </div>
         </div>
     </div>
