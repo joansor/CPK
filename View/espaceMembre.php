a9b28eca3cf760d25e1c65c60ec6b7314a8bc2be
SOR <sor.joan@gmail.com>
2021-03-21 02:20:02 +0100

    Récupération des données pour la partie profil du membre sauf la fonction

--- View/espaceMembre.php
+++ View/espaceMembre.php
@@ -1,92 +1,90 @@
+<h1>Espace Membre</h1>
 <?php
 if (isset($_SESSION['mail']) && $_SESSION["connected"] === true) {
 	$user = $_SESSION['mail'];
 	// afficher un message
 	echo "Bonjour " . $user . ", vous êtes connecté";
+
+	$requete = "SELECT membres.*, representant.nomRepresentant,representant.prenomRepresentant,equipes.EquipeNom FROM `membres` INNER JOIN joue ON joue.Membres_idMembres = membres.idMembres INNER JOIN equipes ON equipes.idEquipe = joue.Equipes_idEquipe INNER JOIN represente ON represente.idMembres = membres.idMembres INNER JOIN representant ON representant.idRepresentant = represente.idRepresentant WHERE membres.mail = '$user'";
+
+	$execute = $dbh->query($requete); // Execute la requête
+	while ($result = $execute->fetch()) { // Parcours les résultats de la requête
 ?>
-	<div id="profil" class="p-5 text-center bg-image" style="margin-left: auto; margin-right: auto; margin-top:100px; background-color: #DADDE8; border: 1px solid #C4C4C4; box-sizing: border-box; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px; height: auto; width:900px;">
-		<img src="/assets/images/imgPhoto.webp" style="display: inline-block; position: absolute; top: 25%; right:25%; height: 135px; width:105px " alt="photo">
-		<table style="display: inline-block; margin-left: auto; margin-right:auto; border-right: 2px solid #e84339;">
-
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 700; padding: 5px; ">Bourne</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 700; padding: 5px; ">Jason</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Date de naissance</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">18/01/2005</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Sexe</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">M</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Adresse</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">22 rue des lilas</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">68260</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Kingersheim</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Mail</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">bourne.jason@wanadoo.fr</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Tel</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">06.00.00.00.00</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Représentant légal</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Bourne Patrick</td>
-			</tr>
-		</table>
-
-
-		<table style="display: inline-block; margin-bottom: auto; margin-left: 3%; margin-right:auto;">
-			<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">N°Licence</td>
-			<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">6815572</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Statut</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Membre</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Fonction</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">/</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Equipe</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Cadets</td>
-			</tr>
-			<tr>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">Classement point</td>
-				<td style="font-family: 'Roboto', sans-serif; font-weight: 400; padding: 5px;">710</td>
-			</tr>
-		</table>
-	</div>
-	<!-- Bloc bouton accés BDD -->
-	<?php if(isset($_SESSION['mail']) && $_SESSION["connected"] === true && $_SESSION['nivResponsabilite'] === "3"){ ?>
-	<div class="p-5 text-center bg-image" style="margin-left: auto; margin-right: auto;">
-		<div>
-			<a href="index.php?page=BDDMembre"><button type="button" class="btn btn-primary" style="display: inline-block;"> Accés gestion base de données des membres</button></a>
+		<div class="d-flex justify-content-center">
+			<div id="profil" class="col-md-9 col-sd-9 row p-4">
+				<div class="col-md-1 col-sd-1 col-lg-1">
+					<img id="photo" src="<?php echo $result['photo'] ?>" alt="photo">
+				</div>
+				<table class="tableEspaceMembre col-md-7 col-sd-7 col-lg-3">
+					<tbody>
+						<tr>
+							<td>Prénom Nom : <?php echo $result['prenomMembre'] . " " . $result['nomMembre'] ?></td>
+						</tr>
+						<tr>
+							<td>Date de naissance : <?php echo $result['dateNaissance'] ?></td>
+						</tr>
+						<tr>
+							<td>Sexe : <?php echo $result['sexe'] ?></td>
+						</tr>
+						<tr>
+							<td>Adresse : <?php echo $result['adresse'] ?></td>
+						</tr>
+						<tr>
+							<td>Code postal : <?php echo $result['cp'] ?></td>
+						</tr>
+						<tr>
+							<td>Ville : <?php echo $result['ville'] ?></td>
+						</tr>
+						<tr>
+							<td>Mail : <?php echo $result['mail'] ?></td>
+						</tr>
+						<tr>
+							<td>Tel : <?php echo $result['telephoneMobile'] ?></td>
+						</tr>
+						<tr>
+							<td>Représentant légal : <?php echo $result['nomRepresentant']." ".$result['prenomRepresentant'] ?></td>
+						</tr>
+					</tbody>
+				</table>
+				<hr id="separation2">
+				<table class="tableEspaceMembre col-md-4 col-sd-4 col-lg-3">
+					<tbody>
+						<tr>
+							<td>N°Licence : <?php echo $result['NLicence'] ?></td>
+						</tr>
+						<tr>
+							<td>Statut : <?php echo $result['MembresComite'] ?></td>
+						</tr>
+						<tr>
+							<td>Fonction :</td>
+						</tr>
+						<tr>
+							<td>Equipe : <?php echo $result['EquipeNom'] ?></td>
+						</tr>
+						<tr>
+							<td>Classement point : <?php echo $result['pointsClassement'] ?></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
-	</div>
+		<!-- Bloc bouton accés BDD -->
+	<?php }
+	if ($_SESSION['mail'] && $_SESSION["connected"] === true && $_SESSION['nivResponsabilite'] === "3") {
+	?>
+		<div class="container p-4 col-md-12 col-sd-12 col-lg-12 d-flex justify-content-center">
+			<div>
+				<a href="index.php?page=BDDMembre"><button type="button" class="btn btn-primary" style="display: inline-block;"> Accés gestion base de données des membres</button></a>
+			</div>
+		</div>
 
 	<?php } ?>
-<?php } else {
-
-?>
-	<div class="pageEspaceMembre">
-		<!-- Titre page Espace Membre -->
-		<h1>Espace Membre</h1>
+<?php } else { ?>
+	<div class="d-flex justify-content-center">
+	<div class="col-md-6 col-sd-6 col-lg-6 mb-5">
 		<!-- Bloc Connexion -->
 		<form action="http://localhost/CPK/index.php?page=traitement" method="post">
-			<div class="p-5 text-center bg-image" style="margin-left: auto; margin-right: auto; background-color: #DADDE8; border: 1px solid #C4C4C4; box-sizing: border-box; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 20px; height: 200px; width:500px">
+			<div id="panelConnexion" class="p-5 text-center bg-image">
 				<div class="form-outline">
 					<input type="email" name="email" id="typeEmail" class="form-control" required />
 					<label class="form-label" for="typeEmail">Identifiant</label>
@@ -104,5 +102,6 @@ if (isset($_SESSION['mail']) && $_SESSION["connected"] === true) {
 				</div>
 			</div>
 		</form>
+		</div>
 	</div>
 <?php } ?>
